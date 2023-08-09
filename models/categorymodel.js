96361900ef450e2a65f2b8462a20f8202f6ae067
vineesh <110625835+vineeshw1994@users.noreply.github.com>
2023-08-09 22:50:49 +0530

    the database models (schemas)
    user, products , category, otp, orders, admin , coupon

---
+++ models/categorymodel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const categorySchema = new mongoose.Schema({
+        categoryID :{
+            type : String,
+        },
+        categoryName :{
+            type : String,
+            unique:true,
+            required : true
+        },
+        categoryDescription :{
+            type : String,
+            required : true
+        },
+       isavilable:{
+        type:Boolean,
+        require:true,
+        default:true
+       }
+        
+})
+
+ const categoryCollection = new mongoose.model("categorydata",categorySchema);
+
+
+module.exports = categoryCollection;
+
+
